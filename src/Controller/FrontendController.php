c8fdfeaa5a66c8c18aad1379e4508833ae183c9a
YaWK <danielretzl@gmail.com>
2023-04-17 18:33:03 +0200

    #2 add FrontendController.php

---
+++ src/Controller/FrontendController.php
@@ -0,0 +1,18 @@
+<?php
+// src/Controller/FrontendController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontendController extends AbstractController
+{
+/**
+* @Route("/", name="homepage")
+*/
+public function index(): Response
+{
+return $this->render('index.html.twig');
+}
+}
