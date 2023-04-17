840fc69e15deb35193dd16226537a0e66f690c37
YaWK <danielretzl@gmail.com>
2023-04-17 16:46:36 +0200

    added admin lte demo page (index only), added asset links

---
+++ src/Controller/admin/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+// src/Controller/AdminController.php
+
+namespace App\Controller\admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin_dashboard")
+     */
+    public function dashboard(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+}
